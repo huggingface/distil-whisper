aeebf59b1b85f39fcf4448e1f4063b8c3fbdf382
nathfavour <116535483+nathfavour@users.noreply.github.com>
2023-11-08 08:38:54 +0000

    new commit

---
+++ stream-whisper.py
@@ -0,0 +1,21 @@
+## PROFILES
+ # CREATE DEFAULT CONTAINER, WITH NO NAME. 
+ # CREATE/SWITCH/LIST PROFILES WITH A CLI HELPER ARGUMENT
+    # input
+    ##cli argument
+
+    ## file list
+
+
+    # process
+    ## is the path audio?
+
+    ## convert to audio
+
+    ## download into the stream directory
+
+        ## should I retain stream file?
+            ## if yes, move every finished file to a cache folder.
+        
+        
+    # output
